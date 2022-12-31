47f026db70cdca0dfce646bc657517bed9060e59
to-my-learning-path <115326302+to-my-learning-path@users.noreply.github.com>
2022-12-31 00:43:22 -0500

    Added third section styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import MastercraftIcon from ".././.././assets/images/logo-mastercraft.svg";
 import BookmarkIcon from "../../assets/images/icon-bookmark.svg";
-import { Divider } from "@mui/material";
+import CloseIcon from "../../assets/images/icon-close-modal.svg";
+import { Divider, useMediaQuery, LinearProgress, Modal } from "@mui/material";
 
 const list = [
   {
@@ -28,12 +29,17 @@ const list = [
 ];
 
 const Home = () => {
+  const isMobile = useMediaQuery("(max-width: 767px)");
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [isModelOpen, setIsModelOpen] = useState(false);
+
   return (
-    <main className=" mx-8 mt-16 space-y-8 ">
+    <main className=" mx-8 mt-16 lg:mt-20 xl:mt-28 space-y-8 max-w-[730px] md:mx-auto">
       <section className=" relative shadow-sm bg-[#fff] rounded-lg p-8 text-center space-y-6">
         <img
           className=" mx-auto absolute -translate-y-14 translate-x-28"
           src={MastercraftIcon}
+          aria-hidden="true"
           alt=""
         />
         <h1 className=" text-xl px-8 font-700 -mt-14">
@@ -42,32 +48,80 @@ const Home = () => {
         <p className=" text-darkGray">
           A beautiful & handcrafted monitor stand to reduce neck and eye strain.
         </p>
-        <div className=" flex justify-between gap-2">
-          <button className=" bg-moderateCyan hover:bg-darkCyan text-[#fff] py-3 px-4 w-full font-500 rounded-full">
+        <div className=" flex justify-between gap-2 items-center">
+          <button
+            onClick={() => setIsModelOpen(!isModelOpen)}
+            className=" bg-moderateCyan hover:bg-darkCyan text-[#fff] py-3 px-4 w-full md:w-fit md:px-12 font-500 rounded-full"
+          >
             Back this project
           </button>
-          <button>
-            <img className=" hover:opacity-80" src={BookmarkIcon} alt="" />
-            <p className=" hidden md:block">Bookmark</p>
-          </button>
+          {isBookmarked ? (
+            <button
+              onClick={() => setIsBookmarked(!isBookmarked)}
+              className="hover:opacity-80 flex justify-between items-center gap-2 md:bg-[#e5e7eb] md:rounded-full pr-4"
+            >
+              <img
+                src={BookmarkIcon}
+                style={{
+                  filter:
+                    "invert(62%) sepia(70%) saturate(353%) hue-rotate(127deg) brightness(85%) contrast(92%)",
+                }}
+                aria-hidden="true"
+                alt=""
+              />
+              <p className=" hidden md:block text-darkCyan font-700">
+                Bookmarked
+              </p>
+            </button>
+          ) : (
+            <button
+              onClick={() => setIsBookmarked(!isBookmarked)}
+              className="hover:opacity-80 flex justify-between items-center gap-2 md:bg-[#e5e7eb] md:rounded-full pr-4"
+            >
+              <img src={BookmarkIcon} aria-hidden="true" alt="" />
+              <p className=" hidden md:block font-700">Bookmark</p>
+            </button>
+          )}
         </div>
       </section>
-      <section className=" bg-[#fff] shadow-sm rounded-lg p-8 text-center space-y-6">
-        <div>
-          <p className=" text-3xl font-700">$89,914</p>
-          <p className=" text-darkGray">of $100,000 backed</p>
+      <section className="bg-[#fff] shadow-sm rounded-lg p-8 text-center space-y-6 ">
+        <div className="  md:flex md:justify-around md:items-center space-y-6 md:space-y-0">
+          <div>
+            <p className=" text-3xl font-700">$89,914</p>
+            <p className=" text-darkGray">of $100,000 backed</p>
+          </div>
+          <Divider
+            varient="middle"
+            aria-hidden="true"
+            flexItem
+            orientation={isMobile ? "horizontal" : "vertical"}
+          />
+          <div>
+            <p className=" text-3xl font-700">5,007</p>
+            <p className=" text-darkGray">total backers</p>
+          </div>
+          <Divider
+            varient="middle"
+            flexItem
+            aria-hidden="true"
+            orientation={isMobile ? "horizontal" : "vertical"}
+          />
+          <div>
+            <p className=" text-3xl font-700">56</p>
+            <p className=" text-darkGray">days left</p>
+          </div>
         </div>
-        <Divider varient="middle" />
-        <div>
-          <p className=" text-3xl font-700">5,007</p>
-          <p className=" text-darkGray">total backers</p>
+        <div className=" text-moderateCyan">
+          <LinearProgress
+            sx={{
+              height: 10,
+              borderRadius: "2rem",
+            }}
+            color="inherit"
+            variant="determinate"
+            value={(89914 / 100000) * 100}
+          />
         </div>
-        <Divider varient="middle" />
-        <div>
-          <p className=" text-3xl font-700">56</p>
-          <p className=" text-darkGray">days left</p>
-        </div>
-        <div></div>
       </section>
       <section className=" bg-[#fff] shadow-sm rounded-lg p-8 text-center">
         <div className=" space-y-6 text-left">
@@ -87,7 +141,7 @@ const Home = () => {
         </div>
         <div className=" space-y-8 mt-8">
           {list.map((item) => (
-            <ModelItem
+            <RewardList
               key={item.title}
               title={item.title}
               price={item.price}
@@ -97,23 +151,55 @@ const Home = () => {
           ))}
         </div>
       </section>
+      <Modal
+        open={isModelOpen}
+        onClose={() => setIsModelOpen(!isModelOpen)}
+        sx={{ overflowY: "scroll" }}
+      >
+        <div className=" my-14 bg-[#fff] shadow-sm rounded-lg p-8 max-w-[730px] md:mx-auto relative">
+          <button
+            onClick={() => setIsModelOpen(!isModelOpen)}
+            className=" absolute top-8 right-8 hover:brightness-100"
+          >
+            <img src={CloseIcon} alt="" />
+          </button>
+          <div className=" space-y-6 text-left">
+            <p className=" text-xl font-700">Back this project</p>
+            <p className=" text-darkGray">
+              Want to support us in bringing Mastercraft Bamboo Monitor Riser
+              out in the world?
+            </p>
+          </div>
+          <div className=" space-y-8 mt-8">
+            {list.map((item) => (
+              <ModelItemList
+                key={item.title}
+                title={item.title}
+                price={item.price}
+                description={item.description}
+                amount={item.amount}
+              />
+            ))}
+          </div>
+        </div>
+      </Modal>
     </main>
   );
 };
 
-const ModelItem = ({ title, price, description, amount }) => {
+const RewardList = ({ title, price, description, amount }) => {
   return (
     <div
       className={` ${
         amount > 0 ? "" : "opacity-60 cursor-not-allowed"
       } border-[1px] border-darkGray text-left p-6 space-y-4 rounded-lg`}
     >
-      <div>
+      <div className=" md:flex md:justify-between md:items-center">
         <p className="text-xl font-700 ">{title}</p>
         <p className=" text-moderateCyan font-500">Pledge ${price} or more</p>
       </div>
       <p className=" text-darkGray">{description}</p>
-      <div className=" space-y-4">
+      <div className=" space-y-4 md:flex md:justify-between md:items-center">
         <p className=" flex items-center gap-2">
           <span className=" text-3xl font-700">{amount}</span> left
         </p>
@@ -126,6 +212,30 @@ const ModelItem = ({ title, price, description, amount }) => {
             Out of Stock
           </button>
         )}
+      </div>
+    </div>
+  );
+};
+
+const ModelItemList = ({ title, price, description, amount }) => {
+  return (
+    <div
+      className={` ${
+        amount > 0 ? "" : "opacity-60 cursor-not-allowed"
+      } border-[1px] border-darkGray text-left p-6 space-y-4 rounded-lg`}
+    >
+      <div className=" md:flex md:justify-between md:items-center">
+        <p className="text-xl font-700 ">{title}</p>
+        <p className=" text-moderateCyan font-500">Pledge ${price} or more</p>
+      </div>
+      <p className=" text-darkGray">{description}</p>
+      <div className=" space-y-4 md:flex md:justify-between md:items-center">
+        <p className=" flex items-center gap-2">
+          <span className=" text-3xl font-700">{amount}</span> left
+        </p>
+        <button className=" bg-moderateCyan hover:bg-darkCyan text-[#fff] py-3 px-8 font-500 rounded-full">
+          Select Reward
+        </button>
       </div>
     </div>
   );
