e47303db5a95507d163c337de86ec7b0a2146991
to-my-learning-path <115326302+to-my-learning-path@users.noreply.github.com>
2022-12-30 19:47:59 -0500

    Added header styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home/Home";
 
+import HeroImageMobile from "./assets/images/image-hero-mobile.jpg";
+import HeroImageDesktop from "./assets/images/image-hero-desktop.jpg";
+
 function App() {
   return (
-    <>
+    <div
+      className={` bg-[url("./assets/images/image-hero-mobile.jpg")] md:bg-[url("./assets/images/image-hero-desktop.jpg")] bg-no-repeat bg-contain`}
+    >
       <Header />
       <Home />
-    </>
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from ".././assets/images/logo.svg";
+import CloseIcon from ".././assets/images/icon-close-menu.svg";
+import OpenIcon from ".././assets/images/icon-hamburger.svg";
+
+import { Backdrop, Divider } from "@mui/material";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <header>
-      <img src={Logo} alt="Crowdfund Logo" />
-      <nav>
-        <ul>
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Discover</a>
-          </li>
-          <li>
-            <a href="">Get Started</a>
-          </li>
-        </ul>
-      </nav>
+    <header className=" flex justify-between p-8 relative">
+      <img className=" object-contain z-10" src={Logo} alt="Crowdfund Logo" />
+      <button className=" z-10 md:hidden" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <img src={CloseIcon} alt="" /> : <img src={OpenIcon} />}
+      </button>
+      <Backdrop
+        className=" absolute min-h-screen"
+        open={isOpen}
+        onClick={() => setIsOpen(false)}
+      >
+        <nav
+          className={` ${
+            isOpen ? "" : "hidden"
+          } bg-[#f8fafc] rounded-lg w-full mx-8 -mt-80 `}
+        >
+          <ul className=" text-black font-500">
+            <li className=" px-6 py-6">
+              <a href="">About</a>
+            </li>
+            <Divider />
+            <li className=" px-6 py-6">
+              <a href="">Discover</a>
+            </li>
+            <Divider />
+            <li className=" px-6 py-6">
+              <a href="">Get Started</a>
+            </li>
+          </ul>
+        </nav>
+      </Backdrop>
     </header>
   );
 };
